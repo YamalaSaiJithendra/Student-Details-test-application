9e64aa1b3eccc5a7245e39ebd48e3c1367851342
srilaxmikatla <153498667+srilaxmikatla@users.noreply.github.com>
2023-12-12 14:26:44 +0530

    added null validations (#6)
    Co-authored-by: Srilaxmi Katla <srilaxmi.katla@sailotech.com>

--- Practice1/Models/StudentModel.cs
+++ Practice1/Models/StudentModel.cs
@@ -8,20 +8,20 @@ namespace Practice1.Models
         public int StudentId { get; set; }
 
         [Required(ErrorMessage = "Name is Required.")]
-        public string Name { get; set; }
+        public string? Name { get; set; }
 
         [Required(ErrorMessage = "Age is required.")]
         public int Age { get; set; }
         public bool Gender { get; set; }
 
         [Required(ErrorMessage = "City Name required.")]
-        public string City { get; set; }
+        public string? City { get; set; }
     }
     public class TeacherModel
     {
-        public string Teacher_Name { get; set; }
+        public string? Teacher_Name { get; set; }
         public int EmpId { get; set; }
-        public string TeachingSubject { get; set; }
+        public string? TeachingSubject { get; set; }
         public int Experience { get; set; }
         public int Phone_Number { get; set; }
         public int Age { get; set; }
